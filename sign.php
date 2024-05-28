2decec19ee688d8d1aa9d7fd3a95302a16a80b5c
GregoryVDev <arexo@hotmail.fr>
2024-05-28 14:17:58 +0200

    create the form for sign up

--- sign.php
+++ sign.php
@@ -9,6 +9,22 @@
 
 <body>
     <h1>Sign up</h1>
+
+    <form method="post">
+        <div>
+            <label for="pseudo">Pseudo</label>
+            <input type="text" name="nickname" id="pseudo">
+        </div>
+        <div>
+            <label for="email">Email</label>
+            <input type="email" name="email" id="email">
+        </div>
+        <div>
+            <label for="pass">Password</label>
+            <input type="password" name="pass" id="pass">
+        </div>
+        <button type="submit">Sign up</button>
+    </form>
 </body>
 
 </html>
