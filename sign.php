a888a9e4ba250c54fc7b35ed6b70d2df89fa55ed
GregoryVDev <arexo@hotmail.fr>
2024-05-28 15:09:49 +0200

    added the password hash in form and require bdd

--- sign.php
+++ sign.php
@@ -1,3 +1,48 @@
+<?php
+
+// On vérifie si le formulaire a été envoyé
+if (!empty($_POST)) {
+    // var_dump($_POST);
+    // Le formulaire a été envoyé
+    // On vérifie que tous les champs requis sont remplis
+    if (
+        isset($_POST["nickname"], $_POST["email"], $_POST["pass"]) && !empty($_POST['nickname']) && !empty($_POST['email']) && !empty($_POST['pass'])
+    ) {
+        // Formulaire complet
+        // On récupère les données en les protégeant
+        $pseudo = strip_tags($_POST['nickname']);
+
+        if (!filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)) {
+            die("L'adresse email est incomplet");
+        }
+
+
+        // On va hasher le mot de passe
+        $pass = password_hash($_POST["pass"], PASSWORD_ARGON2ID);
+
+        // die($pass);
+
+        // On enregistre la bdd
+        require_once("connect.php");
+
+        $sql = "INSERT INTO users (username, email, pass, roles) VALUES (:pseudo, :email, '$pass', '[\"ROLE_USER\"]')";
+
+        $query = $db->prepare($sql);
+
+        $query->bindValue(":pseudo", $pseudo);
+        $query->bindValue(":email", $_POST["email"]);
+
+        $query->execute();
+    } else {
+        // Formulaire pas incomplet
+        die("Le formulaire est incomplet");
+    }
+}
+
+
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
